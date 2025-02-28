3d19d3d3725e233e5665b45142c5e99651b924a4
Telmen17 <telmen7948@gmail.com>
2025-02-27 18:03:21 -0800

    Second initial commit.

---
+++ floor-calculator/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import CalcField from "dir/app/CalcField";
+
+export default function Home() {
+  return (
+    <>
+      <main>
+        <CalcField/>
+      </main>
+    </>
+  );
+}
